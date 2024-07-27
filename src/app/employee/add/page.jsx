f657f39d265ebb8ef153bf2e3e63c443a32bed25
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-07-27 10:00:44 +0530

    pages updates.

--- src/app/employee/add/page.jsx
+++ src/app/employee/add/page.jsx
@@ -1,55 +1,9 @@
-// "use client";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import React, { useState } from "react";
-// import EmployeeForm from "@/components/EmployeeForm";
-// import { fetchData } from "@/utils/api";
-
-// const Page = () => {
-//   const [employee, setEmployee] = useState({
-//     firstName: "",
-//     lastName: "",
-//     emailAddress: "",
-//     phoneNumber: "",
-//     gender: "",
-//     employeeId: "",
-//   });
-
-//   const addEmployeeDetail = async () => {
-//     const data = await fetchData("http://localhost:8000/api/employee", "POST", employee);
-
-//     const { message, error } = data;
-//     if (error) {
-//       alert(error);
-//     } else {
-//       alert(message);
-//       setEmployee({
-//         firstName: "",
-//         lastName: "",
-//         emailAddress: "",
-//         phoneNumber: "",
-//         gender: "",
-//         employeeId: "",
-//       });
-//     }
-//   };
-
-//   return (
-//     <>
-//       <h1>ADD EMPLOYEE DATA</h1>
-//       <EmployeeForm employee={employee} setEmployee={setEmployee} handleSubmit={addEmployeeDetail} mode="add" />
-//     </>
-//   );
-// };
-
-// export default Page;
-
 "use client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import EmployeeForm from "@/components/EmployeeForm";
-import { fetchData } from "@/utils/api";
+import { handleApiError, formatEmployeeData, performApiRequest } from "@/utils/employee-utils";
 import { toast } from "react-toastify";
 
 const SERVER_URL = process.env.NEXT_PUBLIC_API_BASE_URL_POST;
@@ -65,14 +19,10 @@ const Page = () => {
   });
 
   const addEmployeeDetail = async () => {
-    // Convert employeeId to a number if it's not an empty string
-    const formattedEmployee = {
-      ...employee,
-      employeeId: employee.employeeId === "" ? null : Number(employee.employeeId),
-    };
+    const formattedEmployee = formatEmployeeData(employee);
 
     try {
-      const data = await fetchData(SERVER_URL, "POST", formattedEmployee);
+      const data = await performApiRequest(SERVER_URL, "POST", formattedEmployee);
       toast.success(data.message || "The Employee has been Added Successfully.");
       setEmployee({
         firstName: "",
@@ -83,25 +33,7 @@ const Page = () => {
         employeeId: "",
       });
     } catch (error) {
-      let errorMessage;
-
-      try {
-        errorMessage = JSON.parse(error.message);
-      } catch (e) {
-        errorMessage = { general: "An unexpected error occurred" };
-      }
-
-      if (typeof errorMessage === 'string') {
-        toast.error(errorMessage);
-      } else if (typeof errorMessage === 'number') {
-        toast.error(errorMessage.toString());
-      } else if (typeof errorMessage === 'object') {
-        Object.entries(errorMessage).forEach(([field, msg]) => {
-          toast.error(typeof msg === 'string' ? msg : msg.toString());
-        });
-      } else {
-        toast.error("An error occurred");
-      }
+      handleApiError(error);
     }
   };
 
--- src/app/employee/edit/[employeeId]/page.jsx
+++ src/app/employee/edit/[employeeId]/page.jsx
@@ -1,63 +1,10 @@
-// "use client";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import { useParams, useRouter } from "next/navigation";
-// import React, { useEffect, useState } from "react";
-// import EmployeeForm from "@/components/EmployeeForm";
-// import { fetchData } from "@/utils/api";
-
-// const Page = () => {
-//   const router = useRouter();
-//   const { employeeId } = useParams();
-
-//   const [employee, setEmployee] = useState({
-//     firstName: "",
-//     lastName: "",
-//     emailAddress: "",
-//     phoneNumber: "",
-//     gender: "",
-//     employeeId: "",
-//   });
-
-//   const fetchEmployeeData = async (employeeId) => {
-//     const data = await fetchData(`http://localhost:8000/api/employee/${employeeId}`);
-//     setEmployee(data);
-//   };
-
-//   const updateEmployeeData = async () => {
-//     const data = await fetchData(`http://localhost:8000/api/employee/${employeeId}`, "PUT", employee);
-
-//     if (data.error) {
-//       alert(data.error);
-//     } else {
-//       alert(data.message);
-//       router.push("/employee/list");
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (employeeId) {
-//       fetchEmployeeData(employeeId);
-//     }
-//   }, [employeeId]);
-
-//   return (
-//     <>
-//       <h1>UPDATE EMPLOYEE DATA</h1>
-//       <EmployeeForm employee={employee} setEmployee={setEmployee} handleSubmit={updateEmployeeData} mode="edit" />
-//     </>
-//   );
-// };
-
-// export default Page;
-
 "use client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useParams, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import EmployeeForm from "@/components/EmployeeForm";
-import { fetchData } from "@/utils/api";
+import { handleApiError, formatEmployeeData, performApiRequest } from "@/utils/employee-utils";
 import { toast } from "react-toastify";
 
 const SERVER_URL = process.env.NEXT_PUBLIC_API_BASE_URL_POST;
@@ -75,43 +22,20 @@ const Page = () => {
     employeeId: "",
   });
 
-  const fetchEmployeeData = async (employeeId) => {
-    const data = await fetchData(`${SERVER_URL}/${employeeId}`);
+  const fetchEmployeeData = async (id) => {
+    const data = await performApiRequest(`${SERVER_URL}/${id}`, "GET");
     setEmployee(data);
   };
 
   const updateEmployeeData = async () => {
-    // Convert employeeId to a number if it's not an empty string
-    const formattedEmployee = {
-      ...employee,
-      employeeId: employee.employeeId === "" ? null : Number(employee.employeeId),
-    };
+    const formattedEmployee = formatEmployeeData(employee);
 
-    
     try {
-      const data = await fetchData(`${SERVER_URL}/${employeeId}`, "PUT", formattedEmployee);
+      const data = await performApiRequest(`${SERVER_URL}/${employeeId}`, "PUT", formattedEmployee);
       toast.success(data.message || "The Employee has been Updated Successfully.");
       router.push("/employee/list");
     } catch (error) {
-      let errorMessage;
-
-      try {
-        errorMessage = JSON.parse(error.message);
-      } catch (e) {
-        errorMessage = { general: "An unexpected error occurred" };
-      }
-
-      if (typeof errorMessage === 'string') {
-        toast.error(errorMessage);
-      } else if (typeof errorMessage === 'number') {
-        toast.error(errorMessage.toString());
-      } else if (typeof errorMessage === 'object') {
-        Object.entries(errorMessage).forEach(([field, msg]) => {
-          toast.error(typeof msg === 'string' ? msg : msg.toString());
-        });
-      } else {
-        toast.error("An error occurred");
-      }
+      handleApiError(error);
     }
   };
 
