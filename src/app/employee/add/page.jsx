50e4aa829f62fd6fc66b7f2ecceb5844c855e2c0
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-07-27 08:26:38 +0530

    add page env url update

--- src/app/employee/add/page.jsx
+++ src/app/employee/add/page.jsx
@@ -52,6 +52,8 @@ import EmployeeForm from "@/components/EmployeeForm";
 import { fetchData } from "@/utils/api";
 import { toast } from "react-toastify";
 
+const SERVER_URL = process.env.NEXT_PUBLIC_API_BASE_URL_POST;
+
 const Page = () => {
   const [employee, setEmployee] = useState({
     firstName: "",
@@ -70,7 +72,7 @@ const Page = () => {
     };
 
     try {
-      const data = await fetchData("http://localhost:8000/api/employee", "POST", formattedEmployee);
+      const data = await fetchData(SERVER_URL, "POST", formattedEmployee);
       toast.success(data.message || "The Employee has been Added Successfully.");
       setEmployee({
         firstName: "",
