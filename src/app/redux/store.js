b8356fde485fab275a65cdea145b74b442fe6f30
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-07-28 11:08:11 +0530

    redux updates.

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,8 +1,8 @@
-// src/app/store/store.js
+// src/app/redux/store.js
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import viewReducer from './viewSlice';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from './storage'; // Import the custom storage
 
 const rootReducer = combineReducers({
   view: viewReducer,
@@ -17,6 +17,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
