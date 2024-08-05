e33ca2c0306225675c8213ce1197d2fffc28e186
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-05 11:12:40 +0530

    removed test

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '@swc/jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-  testEnvironment: 'jsdom',
-};
