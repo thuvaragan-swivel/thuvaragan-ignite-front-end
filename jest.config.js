f281fbe740deeb5cae680c82e4123d229d975e54
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-06 17:04:00 +0530

    jest config updates

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,15 @@ const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testMatch: ['**/*.test.jsx'],
 };
 
 module.exports = createJestConfig(config);
