6181f4a6191e4badfe846d7d4e7d0ba2742b5c6f
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-14 13:20:53 +0530

    logger updates

--- src/config/loggerConfig.js
+++ src/config/loggerConfig.js
@@ -1,11 +1,11 @@
 import log from "loglevel";
 
-// // Setting the default log level based on environment.
-// if (process.env.NODE_ENV === "production") {
-//   log.setLevel("warn"); // Only logging warnings and errors in production.
-// } else {
-//   log.setLevel("info"); // Logging everything in development.
-// }
+// Setting the default log level based on environment.
+if (process.env.NODE_ENV === "production") {
+  log.setLevel("warn"); // Only logging warnings and errors in production.
+} else {
+  log.setLevel("info"); // Logging everything in development.
+}
 
 // Custom log format similar to Winston
 const originalFactory = log.methodFactory;
